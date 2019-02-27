e0b01aeb65643d2000e9786f479b2b6af99b9652
santhiyakathir <47886883+santhiyakathir@users.noreply.github.com>
2019-02-27 20:07:35 +0530

    Create player_2_11.c

---
+++ player_2_11.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char a[100];
+  scanf("%s",a);
+  if(a[0]=='s'||a[0]=='S')
+  {
+  printf("YES");
+  }
+  else
+  {
+  printf("NO");
+  }
+  getch();
+}
+  
