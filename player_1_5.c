0b08adb0e6ef852bbe0584244b65766d518e9fba
santhiyakathir <47886883+santhiyakathir@users.noreply.github.com>
2019-02-27 16:31:34 +0530

    Create player_1_5.c

---
+++ player_1_5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[100];
+int b[100],n,i,temp,int=0;
+scanf("%s",a);
+l=strlen(a);
+for(i=0;i<l;i++)
+{
+if(a[i]=='I")
+{
+b[i]=1;
+}
+else if(a[i]=='V")
+{
+b[i]=5;
+}
+else if(a[i]=="X")
+{
+b[i]=10;
+}
+else if(a[i]=="L")
+{
+b[i]=50;
+}
+else if(a[i]=="C")
+{
+b[i]=100;
+}
+else if(a[i]=="D")
