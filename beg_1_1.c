c4635f6245f19d16f26c49be639920bab175bbb6
santhiyakathir <47886883+santhiyakathir@users.noreply.github.com>
2019-03-03 11:29:49 +0530

    Create beg_1_1.c

---
+++ beg_1_1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int a;
+scanf("%d",&a);
+if(a>1)
+{
+printf("positive");
+}
+else if(a==0)
+{
+printf("zero");
+}
+else
+{
+printf("negative");
+}
+return 0;
+}
+
+
