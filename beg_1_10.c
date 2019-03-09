4b015f61548d03e83bcecc5bafeed8114fbd5b44
santhiyakathir <47886883+santhiyakathir@users.noreply.github.com>
2019-03-09 12:19:35 +0530

    Update beg_1_10.c

--- beg_1_10.c
+++ beg_1_10.c
@@ -2,8 +2,15 @@
 #include<string.h>
 int main()
 {
-int a[100];
-for(i=0;i<n;i++)
+int a[100],i,count=0;
+for(i=0;a[i]!='\0';i++)
 {
 scanf("%d",&a[i]);
 }
+for(i=0;a[i]!='\0';i++)
+ {
+  count++;
+ }
+printf("%d",count);
+return 0;
+ }
