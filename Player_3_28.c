bdd6ed756cecdbb4685771522f900ffbeb26c6f8
santhiyakathir <47886883+santhiyakathir@users.noreply.github.com>
2019-03-15 20:47:06 +0530

    Update Player_3_28.c

--- Player_3_28.c
+++ Player_3_28.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 #include<string.h>
+int main()
 {
 char a[100];
-
+int 
+gets(a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]==' ')
+{
+a[i]='$';
+}
+}
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]!='$')
+{
+printf("%c",a[i]);
+}
+}
+return 0;
+}
